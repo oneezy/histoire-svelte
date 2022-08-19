a308caa7b4d726b0e895278ea087da1643ba5201
Justin O'Neill <justinoneill2007@gmail.com>
2022-08-18 19:59:43 -0500

    feat: added more stuff better

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-
+/// <reference types="histoire" />
 
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { HstSvelte } from '@histoire/plugin-svelte'
 import { defaultColors } from 'histoire'
 
-export default defineConfig({
+const config = ({
+// export default defineConfig({
+
   plugins: [
     svelte(),
   ],
@@ -23,19 +25,21 @@ export default defineConfig({
       ],
     },
     theme: {
-      title: 'Onezy',
+      title: 'Oneezy',
       logo: {
         square: '/assets/logo.svg',
         light: '/assets/logo.svg',
         dark: '/assets/logo.svg'
       },
       logoHref: '/',
-      favicon: '/assets/favicon.png',
+      favicon: './assets/favicon.png',
 
       colors: {
-        gray: defaultColors.slate,
-        primary: defaultColors.blue
+        gray: defaultColors.gray,
+        primary: defaultColors.blue,
       }
     }
   },
 })
+
+export default config
